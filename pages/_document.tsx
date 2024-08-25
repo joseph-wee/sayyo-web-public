922dde6e5682f71588ffbd7bf61bd34ce05e2222
Wee Yosup <weeyosup@Weeui-iMac.local>
2024-08-25 16:47:05 +0900

    style: 글로벌 폰트 세팅

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,31 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import {
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Sriracha&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:opsz@14..32&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,500&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
