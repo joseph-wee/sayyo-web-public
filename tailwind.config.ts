8ac76b3cf4a55530927de35dcfbc9c2494133d88
Wee Yosup <weeyosup@Weeui-iMac.local>
2024-10-30 17:46:04 +0900

    init: 환경 세팅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,38 @@ const config: Config = {
     fontFamily: {
       Sriracha: ["Sriracha"],
       Inter: ["Inter"],
+      Roboto: ["Roboto"],
+    },
+    colors: {
+      sayyo_primary: "#00A939",
+      sayyo_wht: "#FFFFFF",
+      sayyo_bg: "#F2F6F8",
+      sayyo_l2: "#063322",
+      sayyo_subtext: "#818C97",
+      sayyo_bg_more: "#DEE6EC",
+    },
+    screens: {
+      md720: { max: "720px" },
+      md760: { max: "760px" },
+      md970: { max: "970px" },
+      md1080: { max: "1080px" },
+      md1180: { max: "1180px" },
+      md1450: { max: "1450px" },
+      md1920_720: { max: "1920px", min: "721px" },
+      md1920_760: { max: "1920px", min: "761px" },
+      md1920_970: { max: "1920px", min: "971px" },
+      md1920_1080: { max: "1920px", min: "1081px" },
+      md1920_1180: { max: "1920px", min: "1181px" },
+      md1920_1450: { max: "1920px", min: "1451px" },
+      md1920: { max: "1919px", min: "721px" },
+      tall: { raw: "(max-height: 1151px)" },
+      md805: { max: "805px" },
+    },
+    backgroundImage: {
+      "img-back-middle": "url('/assets/img_back_middle.png')",
     },
   },
   plugins: [],
 };
 export default config;
+3;
