522d748c35a5b5bc5dff6ec41a6a4c20e82bee3e
Joseph <starlightkor@gmail.com>
2025-07-28 10:06:58 +0900

    feat: 포스트, pp, toc 구현, pp랑 toc는 현재 작동안함

---
+++ api/api.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+// real
+axios.defaults.baseURL = "https://api.sayyo.mobileto.io/sayyo/v1/";
+
+// dev
+// axios.defaults.baseURL = "https://dev.sayyo.mobileto.io/sayyo/v1/";
+
+/** 포스트 상세화면 */
+export const apiGetDetailJob = async (id: number) => {
+  try {
+    const res = await axios({
+      method: "GET",
+      url: `get-info-details-job?jobId=${id}`,
+    });
+    return res;
+  } catch (error: any) {
+    console.log(error);
+    return error;
+  }
+};
+
+/** api toc */
+export const apiPostTermsOfCondition = async () => {
+  try {
+    const res = await axios({
+      method: "POST",
+      url: `admin/get-terms-of-condition`,
+    });
+    return res;
+  } catch (error: any) {
+    console.log(error);
+    return error;
+  }
+};
+
+/** api pp */
+export const apiPostPrivacyPolicy = async () => {
+  try {
+    const res = await axios({
+      method: "POST",
+      url: `admin/get-terms-of-condition`,
+    });
+    return res;
+  } catch (error: any) {
+    console.log(error);
+    return error;
+  }
+};
