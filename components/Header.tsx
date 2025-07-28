97fadec7e20124d85241a1c770b044ca2ad77d2e
Joseph <starlightkor@gmail.com>
2025-07-28 09:52:24 +0900

    feat: header 하나로 통합, 한글 반영

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -8,10 +8,47 @@ import { logo_sayyo_small } from "../assets";
 
 /** bg color transparent Header */
 const Header = () => {
+  const router = useRouter();
+  const [page, setPage] = useState("");
+  const [path, setPath] = useState("");
+  const [lang, setLang] = useState("");
+
+  const pathGetter = () => {
+    setPath(router.pathname.substring(3));
+  };
+
+  const pageGetter = () => {
+    if (router.pathname === "/" || router.pathname.includes("main")) {
+      setPage("main");
+      return;
+    }
+    router.pathname !== "/" && setPage("etc");
+  };
+
+  const langGetter = () => {
+    if (router.pathname.includes("en")) {
+      setLang("en");
+      return;
+    }
+    if (router.pathname.includes("ko")) {
+      setLang("ko");
+      return;
+    }
+    setLang("vn");
+  };
+
+  useEffect(() => {
+    pageGetter();
+    pathGetter();
+    langGetter();
+  }, [router]);
+
   return (
-    <header className="absolute z-20 w-[100%]">
+    <header
+      className={`absolute z-20 w-[100%] ${page === "etc" && "bg-sayyo_primary"}`}
+    >
       <div className="m-auto my-0 pl-[24px] pr-[20px] flex items-center justify-between max-w-[1280px] h-[78px] md720:h-[57px] ">
-        <Link href="">
+        <Link href={`${lang === "vn" ? "" : `main`}`}>
           <Image
             src={logo_sayyo_small}
             alt="logo_sayyo_small"
@@ -19,14 +56,32 @@ const Header = () => {
           />
         </Link>
         <div className="flex items-center gap-[17px]">
-          <button className="font-Roboto font-bold text-base text-sayyo_wht underline underline-offset-[5px] md720:text-[12px] md720:leading-[15.6px] md720:underline-offset-[3px]">
-            Vietnamese
-          </button>
+          <Link href={`${page === "main" ? "/" : path}`}>
+            <button
+              className={`${lang === "vn" ? "font-Roboto font-bold text-base text-sayyo_wht underline underline-offset-[5px] md720:text-[12px] md720:leading-[15.6px] md720:underline-offset-[3px]" : "font-Roboto font-normal text-sayyo_wht md720:text-[12px] md720:leading-[15.6px]"} `}
+            >
+              Vietnamese
+            </button>
+          </Link>
+          <div className="w-[1px] h-[13px] border-l-[1px] border-l-[#FFFFFF]/[.4] md720:h-[8.27px]" />
+          <Link
+            href={`${page === "main" ? "/en/main" : lang === "vn" ? `/en${router.pathname}` : `/en${path}`}`}
+          >
+            <button
+              className={`${lang === "en" ? "font-Roboto font-bold text-base text-sayyo_wht underline underline-offset-[5px] md720:text-[12px] md720:leading-[15.6px] md720:underline-offset-[3px]" : "font-Roboto font-normal text-sayyo_wht md720:text-[12px] md720:leading-[15.6px]"} `}
+            >
+              English
+            </button>
+          </Link>
 
           <div className="w-[1px] h-[13px] border-l-[1px] border-l-[#FFFFFF]/[.4] md720:h-[8.27px]" />
-          <Link href="/en/main">
-            <button className="font-Roboto font-normal text-sayyo_wht md720:text-[12px] md720:leading-[15.6px]">
-              English
+          <Link
+            href={`${page === "main" ? "/ko/main" : lang === "vn" ? `/ko${router.pathname}` : `/ko${path}`}`}
+          >
+            <button
+              className={`${lang === "ko" ? "font-Roboto font-bold text-base text-sayyo_wht underline underline-offset-[5px] md720:text-[12px] md720:leading-[15.6px] md720:underline-offset-[3px]" : "font-Roboto font-normal text-sayyo_wht md720:text-[12px] md720:leading-[15.6px]"} `}
+            >
+              Korean
             </button>
           </Link>
         </div>
@@ -98,12 +153,13 @@ const HeaderEn = () => {
 const HeaderGreenEn = () => {
   const router = useRouter();
   const path = router.pathname.substring(3);
+  console.log(path);
 
   return (
     <header className="absolute z-20 w-[100%] bg-sayyo_primary ">
       <div
-        className="m-auto my-0 pl-[24px] pr-[20px] flex items-center justify-between max-w-[1280px] h-[78px] 
-      
+        className="m-auto my-0 pl-[24px] pr-[20px] flex items-center justify-between max-w-[1280px] h-[78px]
+
       md720:h-[57px] "
       >
         <Link href="/en/main">
