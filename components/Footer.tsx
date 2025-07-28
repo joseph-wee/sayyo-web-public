916f8ddf3abc3efb11aaf18b3abc6ee5a0d72954
Joseph <starlightkor@gmail.com>
2025-07-28 09:51:50 +0900

    feat: 푸터 del 삭제, 한글 반영

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,13 +8,13 @@ import { btn_instagram_color, logo_mobileto_black } from "../assets";
 const Footer = () => {
   return (
     <footer
-      className="absolute bottom-0 w-[100%] pt-[52px] pb-[50.22px] bg-sayyo_bg tall:static
-    md1920:pt-[2.708vw] md1920:pb-[2.616vw]
-    md720:pt-[20px] md720:pb-[23px]"
+      className="absolute bottom-0 w-[100%] pt-[53px] pb-[50px] bg-sayyo_bg tall:static
+    md720:pt-[20px] md720:pb-[23px]
+    "
     >
       <div
         className="mb-[9px] flex items-center justify-center gap-[12px]
-      
+
       md720:mb-[6px]"
       >
         <Link
@@ -55,17 +55,17 @@ const Footer = () => {
           </span>
         </button>
       </div>
-      <Link
+      {/* <Link
         href="/del_guide"
-        className="block mb-[9px] text-sayyo_subtext text-center font-Roboto text-[16px] leading-[20.8px] 
+        className="block mb-[9px] text-sayyo_subtext text-center font-Roboto text-[16px] leading-[20.8px]
       md1920_1450:text-[0.833vw] md1920_1450:leading-[1.083vw]
       md1450:text-[12px]
       md720:text-[12px] md720:leading-[15.6px] md720:mb-[27px]"
       >
         Hướng dẫn rút tiền tài khoản
-      </Link>
+      </Link> */}
       <span
-        className="block mb-[40px] text-sayyo_subtext text-center font-Roboto text-[16px] leading-[20.8px] 
+        className="block mb-[40px] text-sayyo_subtext text-center font-Roboto text-[16px] leading-[20.8px]
       md1920:mb-[2.083vw] md1920_1450:text-[0.833vw] md1920_1450:leading-[1.083vw]
       md1450:text-[12px]
       md720:text-[12px] md720:leading-[15.6px] md720:mb-[27px]"
@@ -85,17 +85,17 @@ const Footer = () => {
 const FooterEn = () => {
   return (
     <footer
-      className="absolute bottom-0 w-[100%] pt-[52px] pb-[50.22px] bg-sayyo_bg tall:static
-    md1920:pt-[2.708vw] md1920:pb-[2.616vw]
-    md720:pt-[20px] md720:pb-[23px]"
+      className="absolute bottom-0 w-[100%] pt-[53px] pb-[50px] bg-sayyo_bg tall:static
+    md720:pt-[20px] md720:pb-[23px]
+    "
     >
       <div
         className="mb-[9px] flex items-center justify-center gap-[12px]
-      
+
       md720:mb-[6px]"
       >
         <Link
-          href="/en/pp"
+          href="/pp"
           className="text-sayyo_subtext font-Roboto text-[16px] leading-[20.8px]
           md1920_1450:text-[0.833vw] md1920:leading-[1.083vw]
           md1450:text-[12px]
@@ -105,7 +105,7 @@ const FooterEn = () => {
         </Link>
         <div className="w-[1px] h-[13px] bg-[rgba(0,0,0,0.1)]" />
         <Link
-          href="/en/toc"
+          href="/toc"
           className="text-sayyo_subtext font-Roboto text-[16px] leading-[20.8px]
           md1920_1450:text-[0.833vw] md1920:leading-[1.083vw]
           md1450:text-[12px]
@@ -124,9 +124,7 @@ const FooterEn = () => {
             className="mr-[12px] md720:mr-0"
           />
           <span
-            className=" text-sayyo_subtext font-Roboto text-[16px] leading-[20.8px]
-          
-          md1920_1450:text-[0.833vw] md1920:leading-[1.083vw]
+            className="text-sayyo_subtext font-Roboto text-[16px] leading-[20.8px]     md1920_1450:text-[0.833vw] md1920:leading-[1.083vw]
           md1450:text-[12px]
           md720:text-[12px] md720:leading-[15.6px] md720:hidden"
           >
@@ -134,18 +132,94 @@ const FooterEn = () => {
           </span>
         </button>
       </div>
-      <Link
-        href="/en/del_guide"
-        className="block mb-[9px] text-sayyo_subtext text-center font-Roboto text-[16px] leading-[20.8px] 
+      {/* <Link
+        href="/del_guide"
+        className="block mb-[9px] text-sayyo_subtext text-center font-Roboto text-[16px] leading-[20.8px]
       md1920_1450:text-[0.833vw] md1920_1450:leading-[1.083vw]
       md1450:text-[12px]
       md720:text-[12px] md720:leading-[15.6px] md720:mb-[27px]"
       >
-        Account withdraw guide
-      </Link>
-
+        Hướng dẫn rút tiền tài khoản
+      </Link> */}
       <span
-        className="block mb-[40px] text-sayyo_subtext text-center font-Roboto text-[16px] leading-[20.8px] 
+        className="block mb-[40px] text-sayyo_subtext text-center font-Roboto text-[16px] leading-[20.8px]
+      md1920:mb-[2.083vw] md1920_1450:text-[0.833vw] md1920_1450:leading-[1.083vw]
+      md1450:text-[12px]
+      md720:text-[12px] md720:leading-[15.6px] md720:mb-[27px]"
+      >
+        2024 Sayyo All Rights Reserved.
+      </span>
+      <Image
+        src={logo_mobileto_black}
+        alt="logo_mobileto"
+        className="mx-auto md1920:w-[11.042vw] min-w-[96.2px] md720:w-[96.2px] cursor-pointer"
+        onClick={() => window.open("https://www.mobileto.io")}
+      />
+    </footer>
+  );
+};
+
+const FooterKo = () => {
+  return (
+    <footer
+      className="absolute bottom-0 w-[100%] pt-[53px] pb-[50px] bg-sayyo_bg tall:static
+    md720:pt-[20px] md720:pb-[23px]
+    "
+    >
+      <div
+        className="mb-[9px] flex items-center justify-center gap-[12px]
+
+      md720:mb-[6px]"
+      >
+        <Link
+          href="/pp"
+          className="text-sayyo_subtext font-Roboto text-[16px] leading-[20.8px]
+          md1920_1450:text-[0.833vw] md1920:leading-[1.083vw]
+          md1450:text-[12px]
+          md720:text-[12px] md720:leading-[15.6px]"
+        >
+          개인정보보호
+        </Link>
+        <div className="w-[1px] h-[13px] bg-[rgba(0,0,0,0.1)]" />
+        <Link
+          href="/toc"
+          className="text-sayyo_subtext font-Roboto text-[16px] leading-[20.8px]
+          md1920_1450:text-[0.833vw] md1920:leading-[1.083vw]
+          md1450:text-[12px]
+          md720:text-[12px] md720:leading-[15.6px]"
+        >
+          이용약관
+        </Link>
+        <div className="w-[1px] h-[13px] bg-[rgba(0,0,0,0.1)]" />
+        <button
+          className="flex items-center"
+          onClick={() => window.open("https://www.instagram.com/sayyo.viet")}
+        >
+          <Image
+            src={btn_instagram_color}
+            alt="btn_instagram"
+            className="mr-[12px] md720:mr-0"
+          />
+          <span
+            className="text-sayyo_subtext font-Roboto text-[16px] leading-[20.8px]     md1920_1450:text-[0.833vw] md1920:leading-[1.083vw]
+          md1450:text-[12px]
+          md720:text-[12px] md720:leading-[15.6px] md720:hidden"
+          >
+            Instagram
+          </span>
+        </button>
+      </div>
+      {/* <Link
+        href="/del_guide"
+        className="block mb-[9px] text-sayyo_subtext text-center font-Roboto text-[16px] leading-[20.8px]
+      md1920_1450:text-[0.833vw] md1920_1450:leading-[1.083vw]
+      md1450:text-[12px]
+      md720:text-[12px] md720:leading-[15.6px] md720:mb-[27px]"
+      >
+        Hướng dẫn rút tiền tài khoản
+      </Link> */}
+      <span
+        className="block mb-[40px] text-sayyo_subtext text-center font-Roboto text-[16px] leading-[20.8px]
       md1920:mb-[2.083vw] md1920_1450:text-[0.833vw] md1920_1450:leading-[1.083vw]
       md1450:text-[12px]
       md720:text-[12px] md720:leading-[15.6px] md720:mb-[27px]"
@@ -163,5 +237,6 @@ const FooterEn = () => {
 };
 
 Footer.En = FooterEn;
+Footer.Ko = FooterKo;
 
 export default Footer;
