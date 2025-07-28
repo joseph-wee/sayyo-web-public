86e1b351bd4abde5108391cf9d9b46b401eef55c
Joseph <starlightkor@gmail.com>
2025-07-28 09:47:19 +0900

    feat: 외부 이미지 허용

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.sayyo.mobileto.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dev-static.sayyo.mobileto.io',
+        port: '',
+        pathname: '/**',
+      },
+    ]
+  },
   reactStrictMode: true,
   // output: 'export',
   // trailingSlash: true,
