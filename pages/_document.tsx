754c50e950fb5f251ebb3d8f1ae1821c0cd9bc88
Joseph <starlightkor@gmail.com>
2025-09-22 16:22:48 +0900

    fix: 메타태그 오타 수정 repunch -> sayyo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>Sayyo</title>
-        <meta name="description" content="repunch web" />
+        <meta name="description" content="sayyo web" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           property="og:image"
