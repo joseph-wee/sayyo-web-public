681bbd590ee3e2adca1bb2b1b04b19f672ce6b8e
Joseph <starlightkor@gmail.com>
2026-03-23 11:02:00 +0900

    fix: Html lang vn -> vi 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="vn">
+    <Html lang="vi">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
